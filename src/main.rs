cccc8bcae7014d129fe8fb918764d96863853753
Eric <ericmontelares3@gmail.com>
2020-09-30 16:35:59 -0300

    rust on web

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,6 @@ fn main() {
     let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
     let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     println!("public path: {}", public_path);
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let server = Server::new("0.0.0.0:8080".to_string());
     server.run(WebsiteHandler::new(public_path));
 }
